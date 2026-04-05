77e3bbc8b5a110e0553cbd0b1f2247af3d59e24a
Aryeh Edell <edellaryeh1153@gmail.com>
2026-04-05 15:59:03 -0400

    Initial Next.js app

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h1>ShidduchConnect</h1>
+      <p>My first website is working.</p>
+    </main>
+  );
+}
